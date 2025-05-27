89d9c23bba30fd98a8fc19da5973e142dad3496b
KON-SP <k.spenst@gmail.com>
2025-05-27 10:18:49 +0200

    index and footer adjusted

--- src/footer.js
+++ src/footer.js
@@ -6,9 +6,9 @@ export function renderFooter() {
       <div class="flex flex-col items-center space-y-2">
         <p class="text-sm text-gray-400">This product uses the TMDB API but is not endorsed or certified by TMDB</p>
         <a href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer" class="block w-32">
-          <img src="./pic/TMDB_Logo.svg" alt="TMDB Logo" class="w-1/2 h-auto">
+          <img src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_long_2-9665a76b1ae401a510ec1e0ca40ddcb3b0cfe45f1d51b77a308fea0845885648.svg" alt="TMDB Logo" class="w-full h-auto">
         </a>
-        <p class="text-xs text-gray-500 mt-2">&copy; 2023 Movie Diary. All rights reserved.</p>
+        <p class="text-xs text-gray-500 mt-2">&copy; 2025 Movie Diary. All rights reserved.</p>
       </div>
     `;
 
